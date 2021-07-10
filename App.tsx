49338b69ff2bdc10012b658858de733e4bbee635
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-07-10 14:33:12 -0300

    Soluçao do desafio complementar 2A

--- App.tsx
+++ App.tsx
@@ -4,6 +4,9 @@ import { useFonts, Poppins_400Regular, Poppins_500Medium } from '@expo-google-fo
 import { NavigationContainer } from '@react-navigation/native';
 
 import { AppRoutes } from './src/routes/app.routes';
+import { StorageProvider } from './src/hook/storage';
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/global/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -16,8 +19,12 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <ThemeProvider theme={theme}>
+      <StorageProvider>
+        <NavigationContainer>
+          <AppRoutes />
+        </NavigationContainer>
+      </StorageProvider>
+    </ThemeProvider>
   );
 }
